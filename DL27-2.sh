675d20fbfb0e7960ea99cf57b0fd78b22e78e456
Hubert Palo <darlinnep@gmail.com>
2023-11-15 20:55:53 -0300

    update

--- DL27-2.sh
+++ DL27-2.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=1 python3 hsearch_main.py --data ../data/ --cpu 0.1 --gpu 0.2 --max_concurrent 5 --experiment P10_convae_realworld_thigh_p75
+CUDA_VISIBLE_DEVICES=1 python3 hsearch_main.py --data ../data/ --cpu 0.1 --gpu 0.2 --max_concurrent 3 --experiment P10_convae_realworld_thigh_p75
