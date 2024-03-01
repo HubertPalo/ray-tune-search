3886001f316f2b28f36414601c0f889246cbaf5e
Hubert Palo <darlinnep@gmail.com>
2024-03-01 12:48:27 -0300

    Update exploration_config.py

--- basic/exploration_config.py
+++ basic/exploration_config.py
@@ -11,7 +11,7 @@ class ResourceConfig:
 class SearchSpaceUnit:
     identifier: str
     tune_function: str
-    tune_parameters: List[str]
+    tune_parameters: List[any]
     route: str
     extra_features: Optional[List] = None
 
